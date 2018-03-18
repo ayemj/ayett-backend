7aa37e6b9ff8a15ca50d4d5a6d989a89ecbc9bff
AYE MJ <ayemj.work@gmail.com>
2018-03-18 18:29:28 +0530

    Changed for deployment

--- src/main/java/com/aye/tt/Application.java
+++ src/main/java/com/aye/tt/Application.java
@@ -1,14 +1,27 @@
 package com.aye.tt;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
-    public static void main(final String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+	 public static void main(String[] args) {
+	        SpringApplication.run(Application.class, args);
+	    }
+
+	    @Override
+	    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	        return application.sources(Application.class);
+	    }
 
 }
